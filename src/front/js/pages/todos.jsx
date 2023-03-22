3e8ce1e8436db5c73e5a326b8cb0fb8c896c3f90
estebansolanog <estebansolanoguillen@gmail.com>
2023-03-22 02:19:13 +0000

    Tarea Finalizada

--- src/front/js/pages/todos.jsx
+++ src/front/js/pages/todos.jsx
@@ -1,58 +1,68 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
-export const ToDo = () => {//Esta es la versión con estados centralizados, la razón de usar estados centralizados es poder comunicar estados entre componentes
+export const ToDo = () => {
     const { store, actions } = useContext(Context);
-    const [refresh, setRefresh] = useState(false) //estado del compoenente para controlar su reenderizado
+    const [refresh, setRefresh] = useState(false)
 
     useEffect(() => {
-        //ejecutamos una función asíncrona que traerá la información de la lista de To Do
         const cargaDatos = async () => {
             actions.getToDoList()
         }
         cargaDatos()
         let limpiar = document.querySelector("#tarea")
         limpiar.value = ""
-    }, [store.user, refresh]) //El componente se renderizará la primera vez y cada vez que el estado user o refresh cambien
+    }, [store.user, refresh])
 
     useEffect(() => { console.log(store.todoList) }, [store.todoList])
 
+    let todoCounter = store.todoList.length;
+
     return (
-        <div className="text-center mt-5">
-            Lista de Tareas
+        <div className="text-center container-fluid" id="body">
+            <h1 className="title">My Todos </h1>
             <br />
-            <input placeholder="username" onChange={(e) => { actions.userToDo(e.target.value) }}></input>
-            <br></br>
-            <input placeholder="agrear nueva tarea a la lista" id="tarea"
-                onKeyUp={async (e) => {
-                    if (e.key == "Enter") {
-                        console.log("tarea", e.target.value)
-                        let resultado = await actions.agregarToDo(e.target.value)
-                        if (resultado) {
-                            setRefresh(!refresh)
-                            e.target.value = "" //restauro el valor a vacío
+            <div className="row d-flex justify-content-center mt-0">
+                <input placeholder="Username" id="search-bar" onChange={(e) => { actions.userToDo(e.target.value) }}></input>
+                <br></br>
+                <input placeholder="Agregar ToDo" id="tarea"
+                    onKeyUp={async (e) => {
+                        if (e.key == "Enter") {
+                            console.log("tarea", e.target.value)
+                            let resultado = await actions.agregarToDo(e.target.value)
+                            if (resultado) {
+                                setRefresh(!refresh)
+                                e.target.value = ""
+                            }
                         }
-                    }
-                }}>
-            </input>
-            <br />
-            {store.todoList && store.todoList.length > 0 ? //Verifico el estado
-                <ul>{store.todoList.map((item, index) => { //Hago un map del estado y muestro los to do si existen
-                    return <li key={index}>
-                        {item.label}
-                        <button type="button"    //Agrego un botón para eliminar el todo
-                            onClick={() => {
-                                actions.eliminarToDo(index)	//este botón ejecuta esta acción y le pasamos el índice
-                            }}>
-                            Eliminar
-                        </button>
-                    </li>
-                })}</ul>
-                :
-                <>No hay tareas por hacer o no existe el usuario, presione la tecla ENTER en el input de tareas para crear el usuario</>
-            }
+                    }}>
+                </input>
+                <br />
+            </div>
+            <div className="row d-flex justify-content-center mt-0">
+                {store.todoList && store.todoList.length > 0 ?
+                    <><ul className="p-0 mb-0">{store.todoList.map((item, index) => {
+                        return <li key={index}>
+                            {item.label}
+                            <button type="button"
+                                className="text-danger ocultar mx-3"
+                                onClick={() => {
+                                    actions.eliminarToDo(index);
+                                }}>
+                                X
+                            </button>
+                        </li>;
+                    })}</ul>
+                        <p className="m-0 counter">{todoCounter} ToDo's left</p>
+                        <div className="container-todo-shadow mt-0">
+                            <div className="container-todo-shadow1"></div>
+                            <div className="container-todo-shadow2"></div>
+                        </div></>
+                    :
+                    <>No hay tareas</>
+                }
+            </div>
         </div>
     );
 };
